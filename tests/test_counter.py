39d8f028e3b0cc8c688d6461f3bef380b31fe678
Intensifiesx <hindizain12@yahoo.com>
2024-02-08 02:15:34 -0800

    Fixed the yapping the flake8 did

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -16,7 +16,8 @@ from src.counter import app
 # Import the file that contains the status codes
 from src import status
 
-class CounterTest(TestCase):  
+
+class CounterTest(TestCase):
     """Counter tests"""
 
     def setUp(self):
